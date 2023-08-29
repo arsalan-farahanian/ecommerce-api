34f99ec1ff0c4840b43fd814cff75565de1c8e62
Amirarsalan Farahanian <amirarsalanfarahanian@gmail.com>
2023-08-29 17:22:26 +0330

    add Contact model for ContactUs page

---
+++ models/Contact.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    phone: Number,
+    typeOfRequest: String,
+    message: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+contactSchema.statics.add = async function (newContact) {
+  try {
+    return this.create(newContact);
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = mongoose.model("Contact", contactSchema);
