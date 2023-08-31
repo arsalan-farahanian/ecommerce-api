a2083a4287c1d680117463ed68537c4ac79339a2
Amirarsalan Farahanian <amirarsalanfarahanian@gmail.com>
2023-08-31 18:38:05 +0330

    add route and shop controller for fetching all products or a specific one

---
+++ controllers/shop.js
@@ -0,0 +1,41 @@
+const Product = require("../models/Product");
+
+module.exports = {
+  products_get: async (req, res, next) => {
+    let page = +req.query.page || 1; // getting what page the user is on
+    page = page >= 1 ? page : 1; // preventing explicit negative pages
+
+    let limit = +req.query.limit || 5;
+    limit = limit >= 5 ? limit : 5;
+
+    try {
+      const products = await Product.find()
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .exec();
+
+      res.status(200).json({
+        msg: "products were fetched",
+        products: products,
+        page: page,
+        count: products.length,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  productById_get: async (req, res, next) => {
+    let productId = req.params.id;
+
+    try {
+      let product = await Product.findById(productId);
+      if (!product) {
+        return res.status(404).json({ msg: "Product not found" });
+      }
+      res.status(200).json(product);
+    } catch (err) {
+      next(err);
+    }
+  },
+};
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 //controllers
-const { register_post } = require("./controllers/auth");
+const shopCon = require("./controllers/shop");
+const authCon = require("./controllers/auth");
 const { contactus_post } = require("./controllers/miscellaneous");
 
 //validators
@@ -8,8 +9,12 @@ const register_validator = require("./validators/register_validator");
 const contactus_validator = require("./validators/contactus_validator");
 
 module.exports = () => {
+  //products
+  router.get("/api/product", shopCon.products_get);
+  router.get("/api/product/:id", shopCon.productById_get);
+
   //auth
-  router.post("/api/auth/register", register_validator, register_post);
+  router.post("/api/auth/register", register_validator, authCon.register_post);
 
   router.post("/api/contactus", contactus_validator, contactus_post);
   return router;
