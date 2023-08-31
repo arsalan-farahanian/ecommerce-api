a43c4fe8170b848477bddaa9b0a7e270564b46c4
Amirarsalan Farahanian <amirarsalanfarahanian@gmail.com>
2023-08-31 15:30:04 +0330

    add Product model

---
+++ models/Product.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    basePrice: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    stockQuantity: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    ratings: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        rate: {
+          type: Number,
+          min: 0,
+          max: 5,
+        },
+        review: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    images: [
+      {
+        url: String,
+        altText: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+productSchema.virtual("price").get(function () {
+  return this.basePrice - this.basePrice * (this.discount / 100);
+});
+
+module.exports = mongoose.model("Product", productSchema);
