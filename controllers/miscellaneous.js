f053992408881ffd6c2daf72db4f51bb2596f7c5
Amirarsalan Farahanian <amirarsalanfarahanian@gmail.com>
2023-09-01 16:45:05 +0330

    add route/controller for fetching and deleting a specific contact - still no auth

--- controllers/miscellaneous.js
+++ controllers/miscellaneous.js
@@ -7,18 +7,21 @@ module.exports = {
 
     let limit = +req.query.limit || 5;
     limit = limit >= 5 ? limit : 5;
+    try {
+      let contacts = await Contact.find()
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .exec();
 
-    let contacts = await Contact.find()
-      .limit(limit)
-      .skip((page - 1) * limit)
-      .exec();
-
-    res.status(200).json({
-      msg: "contacts were fetched",
-      contacts: contacts,
-      page: page,
-      count: contacts.length,
-    });
+      res.status(200).json({
+        msg: "contacts were fetched",
+        contacts: contacts,
+        page: page,
+        count: contacts.length,
+      });
+    } catch (err) {
+      next(err);
+    }
   },
   contactus_post: async (req, res, next) => {
     const { name, email, phone, typeOfRequest, message } = req.body;
@@ -31,6 +34,30 @@ module.exports = {
         message,
       });
       res.status(201).json({ msg: "Message sent successfully" });
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  contact_get: async (req, res, next) => {
+    let contactId = req.params.id;
+
+    try {
+      let contact = await Contact.findById(contactId);
+      if (!contact) {
+        return res.status(404).json({ msg: "Contact not found" });
+      }
+      res.status(200).json(contact);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  contact_delete: async (req, res, next) => {
+    let contactId = req.params.id;
+    try {
+      await Contact.findByIdAndDelete(contactId);
+      res.status(204).json({ msg: "Contact was deleted successfully" });
     } catch (err) {
       next(err);
     }
--- routes.js
+++ routes.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 //controllers
 const shopCon = require("./controllers/shop");
 const authCon = require("./controllers/auth");
-const misCon = require("./controllers/miscellaneous");
+const miscCon = require("./controllers/miscellaneous");
 
-//validators
+//user input validators for POST requests
 const register_validator = require("./validators/register_validator");
 const contactus_validator = require("./validators/contactus_validator");
 
@@ -18,8 +18,10 @@ module.exports = () => {
   router.post("/api/auth/register", register_validator, authCon.register_post);
 
   //contact us
-  router.get("/api/contact", misCon.contactus_get); // requires admin auth
-  router.post("/api/contact", contactus_validator, misCon.contactus_post);
+  router.get("/api/contact", miscCon.contactus_get); // requires admin auth
+  router.post("/api/contact", contactus_validator, miscCon.contactus_post);
+  router.get("/api/contact/:id", miscCon.contact_get); //requires admin auth
+  router.delete("/api/contact/:id", miscCon.contact_delete); //requires admin auth
 
   return router;
 };
